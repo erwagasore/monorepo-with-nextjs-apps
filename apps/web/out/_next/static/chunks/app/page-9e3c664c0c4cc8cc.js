44c4a573d13bea863eb04a2eda86659521f310c8
Eugene Rwagasore <rw-g-s-r-@er.local>
2023-05-26 14:29:52 +0200

    Added generated static files for deployment

---
+++ apps/web/out/_next/static/chunks/app/page-9e3c664c0c4cc8cc.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{3299:function(e,r,t){Promise.resolve().then(t.bind(t,9970))},9970:function(e,r,t){"use strict";t.r(r),t.d(r,{Button:function(){return o}});var n=t(1621);t(4132);let o=()=>(0,n.jsx)("button",{onClick:()=>alert("boop"),children:"Shared Boop"})},1379:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(4132),o=Symbol.for("react.element"),u=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};r.jsx=function(e,r,t){var n,i={},s=null,_=null;for(n in void 0!==t&&(s=""+t),void 0!==r.key&&(s=""+r.key),void 0!==r.ref&&(_=r.ref),r)u.call(r,n)&&!c.hasOwnProperty(n)&&(i[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===i[n]&&(i[n]=r[n]);return{$$typeof:o,type:e,key:s,ref:_,props:i,_owner:f.current}}},1621:function(e,r,t){"use strict";e.exports=t(1379)}},function(e){e.O(0,[972,638,744],function(){return e(e.s=3299)}),_N_E=e.O()}]);
